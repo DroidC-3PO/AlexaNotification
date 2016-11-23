ed6a477f469fc119a69d4946446187866f33ff3c
DroidC-3PO <Lu6aC1gaF9vAt4iD5veb8j@von.one>
2016-11-23 11:05:54 +0100

    Description

--- lambda/src/index.js
+++ lambda/src/index.js
@@ -8,15 +8,6 @@
     or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 */
 
-/**
- * This simple sample has no external dependencies or session management, and shows the most basic
- * example of how to create a Lambda function for handling Alexa Skill requests.
- *
- * Examples:
- * One-shot model:
- *  User: "Alexa, tell Greeter to say hello"
- *  Alexa: "Hello World!"
- */
 
 /**
  * App ID for the skill
